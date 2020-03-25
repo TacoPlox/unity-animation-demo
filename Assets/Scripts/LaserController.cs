96ad90fcace45f79610d054ca41295c949128556
Hiram Hernandez <ehiram.hr@gmail.com>
2020-03-25 00:11:51 -0700

    Added demo prefabs, sprites, animations, and scripts.

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    private float timeAlive = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * 1f);
+        timeAlive += Time.deltaTime;
+
+        if (timeAlive > 3f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+    private Animator animator;
+    public GameObject shootingParticles;
+    public float movementSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            transform.Translate(new Vector2(Input.GetAxis("Horizontal") * movementSpeed, Input.GetAxis("Vertical") * movementSpeed));
+        }
+    }
+
+    public void Fire() {
+        Debug.Log("Pew!");
+        GameObject particles = Instantiate(shootingParticles, transform.position + new Vector3(0f, 1f, 0f), transform.rotation);
+        particles.transform.parent = transform;
+    }
+}
